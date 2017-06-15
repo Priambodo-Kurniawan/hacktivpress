add0c3e4449203adc74a3a766df247db62fd9b74
Priambodo-Kurniawan <iam@calibreworks.co.id>
2017-06-15 13:49:57 +0700

    tweak server setup

--- server/app.js
+++ server/app.js
@@ -33,22 +33,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/articles', articles);
 app.use('/api/users', users);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.listen(5000, ()=> {
+  console.log('server is listening at port 5000');
+})
 
 module.exports = app;
